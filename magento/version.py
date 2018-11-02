44429e703765ec04599b27b2709720cb81b027eb
Ryan Moore <ryan.moore@tagglogistics.com>
2018-11-02 16:01:15 -0500

    attempt #1 to get around install errors with unicode

--- magento/version.py
+++ magento/version.py
@@ -1,11 +1,1 @@
-# -*- coding: utf-8 -*-
-"""
-    version
-
-    Just the version here to avoid cyclic dependency when
-    installing the module
-
-    :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
-    :license: BSD, see LICENSE for more details.
-"""
 VERSION = '1.3'
