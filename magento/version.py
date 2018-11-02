b47e28fee03a4ca24cf2f3ada6c02661f64fff4f
Doug Van Horn <dougvanhorn@gmail.com>
2018-11-02 15:15:54 -0500

    Removed non-ascii char.
    Seems to break Python 3.5 pip installs.

--- magento/version.py
+++ magento/version.py
@@ -5,7 +5,7 @@
     Just the version here to avoid cyclic dependency when
     installing the module
 
-    :copyright: © 2013 by Openlabs Technologies & Consulting (P) Limited
+    :copyright: (c) 2013 by Openlabs Technologies & Consulting (P) Limited
     :license: BSD, see LICENSE for more details.
 """
 VERSION = '1.3'
